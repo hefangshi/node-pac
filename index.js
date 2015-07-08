adb26a13c037c6a4f5399dc22804c12209291889
hefangshi <hefangshi@gmail.com>
2015-07-08 15:41:21 +0800

    [upd] throw error if package.json is not matched

--- index.js
+++ index.js
@@ -2,9 +2,4 @@
 
 var NpmStrategy = require('./lib/npm/strategy');
 
-var strategy = new NpmStrategy({
-    mode: 'production',
-    verbose: true
-});
-
-module.exports.strategy = strategy;
+module.exports.NpmStrategy = NpmStrategy;
